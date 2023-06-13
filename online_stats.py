a1c0e4d3ac482b67528b0ef967685c3324bd6876
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-06-13 09:37:25 +0100

    rename package

--- online_stats.py
+++ online_stats.py
@@ -1,12 +1,12 @@
 # author: Nicolas Tessore <n.tessore@ucl.ac.uk>
 # license: MIT
-'''online algorithm for mean, variance, and covariance'''
+"""Online algorithm for mean, variance, and covariance."""
 
-__version__ = '2023.3.23'
+__version__ = "2023.6"
 
 
 def add_sample(n, x, mu, *, var=None, cov=None):
-    '''add sample to online statistics'''
+    """Add sample and compute online statistics."""
     delta = x - mu
     mu += delta/(n+1)
     if var is not None:
